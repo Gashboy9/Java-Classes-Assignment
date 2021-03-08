a8726abad27e66e7488ff2d858c2cc897b1f9bc0
gashboy9 <gashboy@yahoo.com>
2021-03-08 09:05:18 +0000

    Question 6 completed with sample data

--- src/assignments/Q6.java
+++ src/assignments/Q6.java
@@ -4,7 +4,34 @@ public class Q6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Rooms r1 = new Rooms();
+		
+		r1.setData(0, null, null, null);
+		r1.displayData();
 
 	}
 
+}
+
+
+class Rooms {
+	int roomNo;
+	String roomType;
+	String roomArea;
+	String ACmachine;
+	
+	void setData(int rn, String rt, String ra, String ac) {
+		roomNo = rn;
+		roomType = rt;
+		roomArea = ra;
+		ACmachine = ac;
+	}
+	
+	void displayData(){
+		System.out.println("Room number is: " +roomNo);
+		System.out.println("Room type is: " +roomType);
+		System.out.println("Room area is: " +roomArea);
+		System.out.println("AC machine is: " +ACmachine);
+	}
 }
